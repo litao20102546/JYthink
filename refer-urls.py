12d9289c6280b3a9a0380acc4db3a94a5953c143
litao20102546 <626328142@qq.com>
2018-01-03 16:33:04 +0800

    add django learning url
    add django learning url

--- refer-urls.py
+++ refer-urls.py
@@ -1,2 +1,3 @@
 framework: https://www.cnblogs.com/chenice/p/6921727.html
           http://www.runoob.com/django/django-nginx-uwsgi.html
+Django：  https://code.ziqiangxuetang.com/django/django-template2.html
