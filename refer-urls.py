3fa690333bde83922895ac6c03035d4834c1a7f4
litao20102546 <626328142@qq.com>
2018-01-11 00:18:14 +0800

    add url
    add url

--- refer-urls.py
+++ refer-urls.py
@@ -3,3 +3,21 @@ framework: https://www.cnblogs.com/chenice/p/6921727.html
 Django：  https://code.ziqiangxuetang.com/django/django-template2.html
           http://python.usyiyi.cn/translate/django_182/topics/db/models.html
 Nginx:    http://www.nginx.cn/doc/index.html
+浏览网站记录：
+Mac下安装及配置Eclipse  https://www.cnblogs.com/quickcodes/p/5399385.html
+Python搭建网站学习： http://edu.51cto.com//center/course/lesson/index?id=48050
+WEB IDE 搭建： https://zhuanlan.zhihu.com/p/22430099
+OJ搭建 https://github.com/QingdaoU/OnlineJudge
+账号注册登录：http://baidu.iqiyi.com/watch/3271427526277209049.html?&recFrom=site&list=2&&page=videoMultiNeed
+django 学习：http://so.iqiyi.com/so/q_python_django_web%E5%BC%80%E5%8F%91?source=input&sr=263058856285
+
+OJ Python测试题 http://www.pythontip.com/acm/problemCategory
+https://github.com/QingdaoU/OnlineJudgeDeploy
+http://docs.onlinejudge.me/#/
+https://github.com/QingdaoU/OnlineJudge
+
+了解比赛内容：http://www.noi.cn/
+
+
+Gmail和youtube都是python的。国内的豆瓣也是Python的
+
